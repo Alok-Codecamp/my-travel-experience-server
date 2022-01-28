d19a2b54f3624357c71bf7e2520182e85ae1f912
Alok Kumar <alok61.bd@gmail.com>
2022-01-29 00:20:47 +0600

    database created

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
-const express =require ('express');
-const app=express();
-const port=process.env.PORT||5000;
+const express = require('express');
+const { MongoClient, Db } = require('mongodb');
+const ObjectId=require('mongodb').ObjectId;
 const cors=require('cors')
-app.use(cors());
+const app = express();
+const port = process.env.PORT || 5000;
+require('dotenv').config();
 
-app.get('/',(req,res)=>{
-    res.send('my-travel-experienc')
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mrwnd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run(){
+    try{
+        await client.connect();
+        console.log("database connected successfully");
+
+        const database=client.db("my-travel-blog");
+        const blogCollection=database.collection("blog");
+
+        app.get('/blogs',async(req,res)=>{
+          const cursor=blogCollection.find({});
+          const blogs=await cursor.toArray();
+          res.send(blogs)
+        })    
+     
+    } 
+    finally{
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('wellcome to travel app!')
 })
- 
-app.listen(port,()=>{
-    console.log('my-travel-experience server running')
+
+app.listen(port, () => {
+  console.log(` my travel app listening at Port:${port}`)
 })
